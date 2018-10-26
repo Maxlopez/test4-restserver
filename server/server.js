910bfb8c3a10f5652c7acc91f528630afb1b9066
Max <lopezmax.p@gmail.com>
2018-10-25 20:50:07 -0500

    conexion mLab

--- server/server.js
+++ server/server.js
@@ -14,9 +14,7 @@ app.use(bodyParser.json())
 app.use(require('./routes/usuario'));
 
 
-
-
-mongoose.connect('mongodb://localhost:27017/cafe',  {useNewUrlParser: true },(err, res) => {
+mongoose.connect(process.env.URLDB,  {useNewUrlParser: true },(err, res) => {
     if( err ){
         throw err;
         return;
